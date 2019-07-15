4a75c84401509efce8170b53578dff89a820982c
Kenny Grossman <mrkengrossman@gmail.com>
2019-07-15 08:40:31 -0700

    Uploading to git so I can work while away today. Does not run in this state.

--- DODInvestmentBot.py
+++ DODInvestmentBot.py
@@ -2,26 +2,44 @@
 #This project is intended to be used for interview purposes only
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-if __name__ == "__main__":
-	#Add location of downloaded selenium browser drivers
-	#Drivers can be found at https://www.seleniumhq.org/download/
-	driverFolderPath = ''
-	URL = 'https://apps.dtic.mil/dodinvestment/#/advancedSearch'
-	searchString = '0603680F'
+#Add location of downloaded selenium chrome driver
+#Drivers can be found at https://www.seleniumhq.org/download/
+class pageModel:
+	def setUp(self):
+		print("here")
+		driverFolderPath = '/Users/kennygrossman/Projects/Bots/Drivers/chromedriver'
+		URL = 'https://apps.dtic.mil/dodinvestment/#/advancedSearch'
+		webpageTimeout = 10
+		self.searchString = '0603680F'
+		
+		self.driver = webdriver.Chrome(driverFolderPath)
+		self.driver.get(URL)
 
-	#Setup drivers
-	drivers = {
-		"chrome"	: webdriver.Chrome(driverFolderPath),
-		"firefox"	: webdriver.Firefox(driverFolderPath),
-		"ie"		: webdriver.Ie(driverFolderPath),
-	}
-
-	#Test with all available browser drivers
-	for driver in drivers:
-		#Add try catch to ensure drivers are found before proceeding
+		#Wait for page and content to load before proceeding
 		try:
-			driver.get(URL)
-
+		    element = WebDriverWait(driver, webpageTimeout).until(
+		        EC.presence_of_element_located((By.XPATH, '//*[@id="budgetNumObj_value"]'))
+		    )
 		except:
-			#Fail all remaining test and move on
+			print("Failed to load webpage")
+			self.driver.Dispose()
+		else:
+			#Identify page objects for later use
+			self.searchButton = driver.find_element(By.XPATH, '//*[@id="performSearch"]')
+			self.clearButton = driver.find_element(By.XPATH, '//*[@id="clearSearch"]')
+			self.programSearch = driver.find_element(By.XPATH, '//*[@id="budgetNumObj_value"]')
+			self.budgetYearsDropDownItems = driver.find_elements(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[1]/am-multiselect/div/ul/li')
+			self.agenciesDropDownItems = driver.find_elements(By.XPATH, '')
+
+			for element in budgetYearsDropDownItems:
+				print(element.outerText)
+
+	def searchTest(self, searchString):
+		programElementNumberSearch = driver.find_elements(By.XPATH, '//button')
+
+	def __init__(self):
+		setUp()
