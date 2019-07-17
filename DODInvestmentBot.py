c7a815f50f041c98f2cc83565734422a88f64be8
Kenny Grossman <mrkengrossman@gmail.com>
2019-07-16 19:01:04 -0700

    Fixed bugs and added a new test case

--- DODInvestmentBot.py
+++ DODInvestmentBot.py
@@ -1,5 +1,6 @@
 #Created by Kenny Grossman July, 2019
 #This project is intended to be used for interview purposes only
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,7 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 '''
 Helper Functions 
 '''
-
 #Function for more visible test headers
 def printTestName(message):
 	bufferTiles = 24 - (int)(len(message)/2)
@@ -19,7 +19,6 @@ def printTestName(message):
 #Clear screen to prep for testing
 def testCleanup():
 	clearButton.click()
-	# print("Clearing screen for next test")
 
 #Search string using program search return boolean value
 def programSearch(searchString):
@@ -28,40 +27,81 @@ def programSearch(searchString):
 
 	#Wait for search results to appear
 	try:
+		#TODO: MAKE THIS WORK ON SINGLE PAGE RESULTS
 	    element = WebDriverWait(driver, 5).until(
-	        EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/ng-include/div/dir-pagination-controls[1]/div/ul/li[3]/a'))
+	        EC.presence_of_element_located((By.CLASS_NAME, 'record-listing'))
 	    )
 	except:
 		return False
 	else:
 		return True
+
 #Grab all search results from all pages and return list of objects
 def collectSearchResults(searchString):
 	if (programSearch(searchString)):
-		nextPageButtonContainer = driver.find_element(By.XPATH, '/html/body/div[3]/div/ng-include/div/dir-pagination-controls[1]/div/ul/li[5]')
-		nextPageButton = driver.find_element(By.XPATH, '/html/body/div[3]/div/ng-include/div/dir-pagination-controls[1]/div/ul/li[5]/a')
+		try:
+			nextPageButtonContainer = driver.find_element(By.XPATH, '/html/body/div[3]/div/ng-include/div/dir-pagination-controls[1]/div/ul/li[5]')
+			nextPageButton = driver.find_element(By.XPATH, '/html/body/div[3]/div/ng-include/div/dir-pagination-controls[1]/div/ul/li[5]/a')
+		except:
+			pass
+
 		searchResults = []
 
 		#Loop through all pages
 		hasNextPage = True
 		while(hasNextPage):
 			#Grab all results and add to searchResult list
-			searchResults.extend(driver.find_elements(By.CLASS_NAME, 'record-listing'))
-			
+			tempResults = driver.find_elements(By.CLASS_NAME, 'record-listing')
+
+			for result in tempResults:
+				searchResults.append(result.get_property('innerText'))
+
 			#Determine if another page exists
-			buttonClasses = nextPageButtonContainer.get_attribute( "class" ).split(' ')
-			hasNextPage = "disabled" not in buttonClasses
+			try:
+				buttonClasses = nextPageButtonContainer.get_attribute( "class" ).split(' ')
+				hasNextPage = "disabled" not in buttonClasses
+			except:
+				hasNextPage = False
 			
 			if(hasNextPage):
 				nextPageButton.click()
+				time.sleep(.5)
+
 		return searchResults
 	else:
 		print('Fail: no search results found')
 		return []
 
 '''
+Helper functions that have not yet been implemented
+'''
+def revealAndClickBudetOptions():
+	#Reveal Drop down for budget 
+	budgetYearsDropDownButton.click()
+
+	#Print and click all buttons in drop down
+	for element in budgetYearsDropDownItems:
+		print(element.get_property("outerText"))
+		element.click()
+
+def revealAndClickBudetOptions():
+	#Reveal Drop down for agencies
+	agenciesYearsDropDownButton.click()
+
+	#Print and click all buttons in drop down
+	for element in agenciesYearsDropDownItems:
+		print(element.get_property("outerText"))
+		element.click()	
+
+'''
 Test Functions
 '''
+def testSuite(searchString):
+	searchTest(searchString)
+	searchCorrectnessTest(searchString)
+	validateNumberofResults(searchString)
+	validateLinkAccuracy(searchString)
+	clearButtonTest()
 
 #Attempt to search in program element number search field
 def searchTest(searchString):
@@ -83,9 +123,10 @@ def searchCorrectnessTest(searchString):
 
 	#Loop through search results to verify search String appears in each result
 	testPassed = True if len(searchResults) > 0 else False;	
-	
+
 	for result in searchResults:
-		if searchString not in result.get_property('innerText'):
+		# print('Result: {}'.format(result.get_property('innerText')))
+		if searchString not in result:
 			testPassed = False
 
 	#Print test results
@@ -94,18 +135,55 @@ def searchCorrectnessTest(searchString):
 	else:
 		print('Fail: Search term did not appear in all results')
 
-# def validateNumberofResults()
-# 	#Compare numberOfSearchResults to 
-# 	testPassed = True if numberOfSearchResults == 
+#Ensure the correct number of results appear
+def validateNumberofResults(searchString):
+	testCleanup()
+	printTestName('Starting Validate Number of Results Test')	
+	searchResults = collectSearchResults(searchString)
+	
+	#Strip "Results: " from string leaving only the integer
+	resultsValue = int(resultsNumber.get_property("outerText").strip("Results: "))
+	
+	testPassed = True if resultsValue == len(searchResults) else False
+	print('Found {} results and expected {} results'.format(len(searchResults), resultsValue))
 
+	#Print test results
+	if testPassed:
+		print('Pass: Number of results are accurate')
+	else:
+		print('Fail: Search provided a different number of results than expected')	
 
+#Validate that links on page match filenames
+def validateLinkAccuracy(searchString):
+	printTestName('Validate links match filenames')	
+	testCleanup()
+	searchResults = collectSearchResults(searchString)
+	
+
+#Ensure clear button clears text field and results
+def clearButtonTest():
+	printTestName('Test Clear Button functionality')	
+	testCleanup()
+	if programSearchBox.get_property("value") == "" and resultsNumber.get_property("outerText") == "Results: ":
+		testPassed = True
+	else:
+		testPassed = False
+
+	#Print test results
+	if testPassed:
+		print('Pass: Clear Button Successfully cleared program element number search field')
+	else:
+		print('Fail: Clear Button did not reset some elements')	
+
+'''
+Main Body
+'''
 #Add location of downloaded selenium chrome driver
 #Drivers can be found at https://www.seleniumhq.org/download/
 driverPath = '/Users/kennygrossman/Projects/Bots/Drivers/chromedriver'
 URL = 'https://apps.dtic.mil/dodinvestment/#/advancedSearch'
 webpageTimeout = 30
 searchString = '0603680F'
-numberOfSearchResults = 0
 
 #Initialize Chrome driver and load webpage
 driver = webdriver.Chrome(driverPath)
@@ -118,36 +196,17 @@ try:
     )
 except:
 	print("Failed to load webpage")
-	driver.dispose()
 else:
 	#Identify page objects for later use
 	searchButton = driver.find_element(By.XPATH, '//*[@id="performSearch"]')
 	clearButton = driver.find_element(By.XPATH, '//*[@id="clearSearch"]')
 	programSearchBox = driver.find_element(By.XPATH, '//*[@id="budgetNumObj_value"]')
 	resultsNumber = driver.find_element(By.XPATH, '//*[@id="navbarCollapse"]/ul/li[2]/a/strong')
-	# budgetYearsDropDownButton = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[1]/am-multiselect/div/button')
-	# budgetYearsDropDownItems = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[1]/am-multiselect/div/ul').get_property("children")
-	# agenciesYearsDropDownButton = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[2]/am-multiselect/div/button')
-	# agenciesYearsDropDownItems = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[2]/am-multiselect/div/ul').get_property("children")
+	budgetYearsDropDownButton = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[1]/am-multiselect/div/button')
+	budgetYearsDropDownItems = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[1]/am-multiselect/div/ul').get_property("children")
+	agenciesYearsDropDownButton = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[2]/am-multiselect/div/button')
+	agenciesYearsDropDownItems = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/form/div[3]/div[2]/am-multiselect/div/ul').get_property("children")
 
-
-	searchTest(searchString)
-	searchCorrectnessTest(searchString)
-
-
-
-	# #Reveal Drop down for budget 
-	# budgetYearsDropDownButton.click()
-
-	# #Print and click all buttons in drop down
-	# for element in budgetYearsDropDownItems:
-	# 	print(element.get_property("outerText"))
-	# 	element.click()
-
-	# #Reveal Drop down for agencies
-	# agenciesYearsDropDownButton.click()
-
-	# #Print and click all buttons in drop down
-	# for element in agenciesYearsDropDownItems:
-	# 	print(element.get_property("outerText"))
-	# 	element.click()	
+	#Run Test Suite
+	testSuite(searchString)
+	
